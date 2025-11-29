fda4ded8c7d5056e1c677be15ca653df5c9732b2
maksim <maksimhhfjjfjf@gmail.com>
2025-11-29 10:25:41 +0300

    PZ.2

---
+++ PZ_2/PZ_2.1.py
@@ -0,0 +1,20 @@
+#Дано двузначное число.Вывести число , полученное при перестановке цифр исходного числа.
+try:
+
+    n = int(input("введите двузначное число: "))
+
+    if 10 <= n <= 99:
+
+        first_digit = n // 10
+
+        second_digit = n % 10
+
+        reversed_number = second_digit * 10 + first_digit
+
+        print("число после перестановки:", reversed_number)
+    else:
+
+        print("ошибка: введите именно двузначное число")
+except ValueError:
+
+    print("ошибка: введите целое число")
